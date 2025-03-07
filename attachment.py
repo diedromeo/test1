8616b9afe91d720f49f6df74a7fa1ba3a211ca64
diedromeo <119026435+diedromeo@users.noreply.github.com>
2025-03-07 09:00:40 +0530

    test

---
+++ attachment.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+def decode(x):
+    if x == "minuteman kang":
+        f = "byteme{this_is_a_test_flag}"
+        return f
+    else:
+        return "A god doesn't PLEAD!! Try again."
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    message = ""
+    if request.method == 'POST':
+        password = request.form.get('password', '')
+        message = decode(password)
+    return render_template('index.html', message=message)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
